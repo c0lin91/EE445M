de8df39be6931ee7890881020988f24260fced44
c0lin91 <colin.hickman@utexas.edu>
2014-02-24 18:03:43 -0600

    First Commit
    Trying stuff out

---
+++ Lab3/Interpreter.h
@@ -0,0 +1,9 @@
+#ifndef INTERPRETER_H
+#define INTERPRETER_H
+void sendCommand(char* cmdString);
+
+int skipLeadingSpace(char* string);
+
+
+#endif
+
---
+++ Lab3/adc.d
@@ -0,0 +1,4 @@
+.\adc.o: ADC.c
+.\adc.o: ADC.h
+.\adc.o: lm4f120h5qr.h
+.\adc.o: OS.h
